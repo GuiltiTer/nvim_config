718e7968514940b769834c1bc77cf566478fb6fd
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-04-06 16:13:12 +0330

    Add mini.starter plugin

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,23 +1,61 @@
-local logo = {
-  "                 ▄   ▄                 ",
-  "             ▄█▄ █▀█▀█ ▄█▄             ",
-  "            ▀▀████▄█▄████▀▀            ",
-  "                 ▀█▀█▀                 ",
-  "                                       ",
-  "                                       ",
-  "    █▀▀ █░█ █ █░░ ▀█▀ █ ▀█▀ █▀▀ █▀█    ",
-  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀",
-  "    █▄█ █▄█ █ █▄▄ ░█░ █ ░█░ ██▄ █▀▄    ",
-}
-
 return {
-  "goolord/alpha-nvim",
-  opts = function(_, opts)
-    opts.section.header.val = logo
-    opts.section.buttons.val = {
-      opts.button("LDR o", "  " .. "File Manager"),
-      opts.button("LDR f T", "  " .. "TODOs"),
-    }
-  end,
-  enabled = false,
+  { "goolord/alpha-nvim", enabled = false },
+  {
+    "echasnovski/mini.starter",
+    version = "*",
+    config = function()
+      local starter = require "mini.starter"
+      local hooks = {
+        clear_statusline = function(buff)
+          vim.opt_local.statusline = " "
+          vim.cmd "highlight StatusLine ctermbg=None guibg=None"
+          return buff
+        end,
+
+        local_keymap = function(buff)
+          vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = 0 })
+          vim.keymap.set("n", "j", "<cmd>normal <Down><cr>", { buffer = 0 })
+          vim.keymap.set("n", "k", "<cmd>normal <Up><cr>", { buffer = 0 })
+          return buff
+        end,
+      }
+
+      starter.setup {
+        autoopen = true,
+        silent = true,
+        evaluate_single = true,
+
+        items = {
+          {
+            { name = "Files", action = "Telescope find_files", section = "" },
+            { name = "Git", action = function() require("astrocore").toggle_term_cmd "lazygit" end, section = "" },
+            { name = "Diff", action = "DiffviewOpen", section = "" },
+            { name = "TODO", action = "Telescope todo-comments", section = "" },
+            { name = "Explorer", action = function() require("tfm").open() end, section = "" },
+          },
+        },
+
+        header = "",
+
+        content_hooks = {
+          starter.gen_hook.adding_bullet(),
+          starter.gen_hook.aligning("center", "center"),
+          hooks.clear_statusline,
+          hooks.local_keymap,
+        },
+
+        query_updaters = "abcdefghilmnopqrstuvwxyz0123456789_-.",
+      }
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimStarted",
+        callback = function()
+          local stats = require("lazy").stats()
+          local padding = "░ "
+          starter.config.footer = padding .. stats.loaded .. "/" .. stats.count .. "  "
+          pcall(starter.refresh)
+        end,
+      })
+    end,
+  },
 }
