470cb65340630c492355216dc161eaac46f23c9e
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-04-16 20:23:54 +0330

    Refactor neovide configs

--- lua/neovide/init.lua
+++ lua/neovide/init.lua
@@ -1,6 +1,6 @@
-Neovide_default_font_size = 16
-Neovide_font_size_adjust_step = 2
-vim.o.guifont = "Cascadia Code:h" .. Neovide_default_font_size
+vim.g.my_neovide_default_font_size = 16
+vim.g.my_neovide_font_size_adjust_step = 2
+vim.o.guifont = "Cascadia Code:h" .. vim.g.my_neovide_default_font_size
 vim.o.termguicolors = true
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_theme = require("user.env").colorscheme
--- lua/neovide/mappings.lua
+++ lua/neovide/mappings.lua
@@ -9,21 +9,21 @@ local set_font = function(font_family, font_size) vim.o.guifont = font_family ..
 
 local inc_font_size = function()
   local font_family, current_size = get_font_prop()
-  local new_size = current_size + Neovide_font_size_adjust_step
+  local new_size = current_size + vim.g.my_neovide_font_size_adjust_step
   if new_size > 40 then return end
   set_font(font_family, new_size)
 end
 
 local dec_font_size = function()
   local font_family, current_size = get_font_prop()
-  local new_size = current_size - Neovide_font_size_adjust_step
+  local new_size = current_size - vim.g.my_neovide_font_size_adjust_step
   if new_size < 2 then return end
   set_font(font_family, new_size)
 end
 
 local reset_font_size = function()
   local font_family, _ = get_font_prop()
-  set_font(font_family, Neovide_default_font_size)
+  set_font(font_family, vim.g.my_neovide_default_font_size)
 end
 
 vim.keymap.set("n", "<D-=>", inc_font_size)
