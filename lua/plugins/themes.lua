b1fea9487647026f6a89fbafbcd77b9fb0f84a3c
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-04-19 12:06:16 +0330

    Load some themes eagerly

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,10 +1,11 @@
 return {
+  { "projekt0n/github-nvim-theme", priority = 1000, lazy = false },
+  { "folke/tokyonight.nvim", priority = 1000, lazy = false },
+  { "Shatur/neovim-ayu", priority = 1000, lazy = false },
+
   { "catppuccin/nvim", priority = 1000, lazy = true, name = "catppuccin" },
   { "dracula/vim", priority = 1000, lazy = true, name = "dracula" },
   { "marko-cerovac/material.nvim", priority = 1000, lazy = true },
   { "nyoom-engineering/oxocarbon.nvim", priority = 1000, lazy = true },
-  { "folke/tokyonight.nvim", priority = 1000, lazy = true },
   { "astrotheme", priority = 1000, lazy = true },
-  { "projekt0n/github-nvim-theme", priority = 1000, lazy = true },
-  { "Shatur/neovim-ayu", priority = 1000, lazy = true },
 }
