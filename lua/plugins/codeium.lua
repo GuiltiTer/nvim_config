e49ff80c62dcf299390750d85c56822fa963750b
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-04-20 19:42:47 +0330

    Modify codeium config to use cmp

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,6 +1,15 @@
 return {
   "Exafunction/codeium.nvim",
-  cmd = "Codeium",
-  build = ":Codeium Auth",
-  opts = {},
+  event = "InsertEnter",
+  config = function()
+    require("codeium").setup()
+    local config = require("cmp").get_config()
+
+    table.insert(config.sources, 1, {
+      name = "codeium",
+      group_index = 1,
+      priority = 100,
+      format = require("lspkind").cmp_format { symbol_map = { Codeium = "" } },
+    })
+  end,
 }
