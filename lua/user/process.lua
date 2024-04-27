ed4ffa0c971ee8077e0f44f5e52ad99ade58294a
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-04-27 22:11:52 +0330

    Add notify stdio to process spawn

--- lua/user/process.lua
+++ lua/user/process.lua
@@ -1,18 +1,38 @@
 ---@param opts {cmd: string, args: string[], on_exit: fun(code: number, signal: number) , silent: boolean}
 local function spawn(opts)
   local handle, pid
+  local stdout, stderr = vim.loop.new_pipe(false), vim.loop.new_pipe(false)
+  local strout, strerr = "", ""
 
-  if not opts.on_exit then
-    opts.on_exit = function(code, _)
-      if not opts.silent then print("Exit code: " .. code) end
-      if handle then handle:close() end
+  local on_exit = function(code, signal)
+    if opts.on_exit then opts.on_exit(code, signal) end
+
+    if stdout then
+      stdout:read_stop()
+      stdout:close()
     end
+    if stderr then
+      stderr:read_stop()
+      stderr:close()
+    end
+
+    if not opts.silent then
+      vim.notify(strerr, vim.log.levels.ERROR)
+      vim.notify(strout, vim.log.levels.INFO)
+    end
+
+    if handle then handle:close() end
   end
 
-  handle, pid = vim.loop.spawn(opts.cmd, {
-    args = opts.args,
-    stdio = { nil, vim.loop.new_pipe(false), vim.loop.new_pipe(false) },
-  }, opts.on_exit)
+  handle, pid = vim.loop.spawn(opts.cmd, { args = opts.args, stdio = { nil, stdout, stderr } }, on_exit)
+
+  if stderr then stderr:read_start(function(_, data)
+    if data then strerr = strerr .. data end
+  end) end
+  if stdout then stdout:read_start(function(_, data)
+    if data then strout = strout .. data end
+  end) end
+
   return handle, pid
 end
 
