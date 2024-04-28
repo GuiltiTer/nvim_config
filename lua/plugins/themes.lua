b9f2a0bad8fd645b166fb2ad7c046477ab668fcb
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-04-28 23:30:20 +0330

    Add eldritch theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,6 +2,7 @@ return {
   { "projekt0n/github-nvim-theme", priority = 1000, lazy = false },
   { "folke/tokyonight.nvim", priority = 1000, lazy = false },
   { "Shatur/neovim-ayu", priority = 1000, lazy = false },
+  { "eldritch-theme/eldritch.nvim", priority = 1000, lazy = false },
 
   { "catppuccin/nvim", priority = 1000, lazy = true, name = "catppuccin" },
   { "dracula/vim", priority = 1000, lazy = true, name = "dracula" },
