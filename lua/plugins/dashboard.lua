3bfbcb5f1c53b3dbe0578f79ac561f5df201770a
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-07-18 16:51:49 +0330

    update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,59 +1,23 @@
 return {
-  {
-    "echasnovski/mini.starter",
-    -- version = "*",
-    config = function()
-      local starter = require "mini.starter"
-      local hooks = {
-        clear_statusline = function(buff)
-          vim.opt_local.statusline = " "
-          vim.cmd "highlight StatusLine ctermbg=None guibg=None"
-          return buff
-        end,
-
-        local_keymap = function(buff)
-          vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = 0 })
-          vim.keymap.set("n", "j", "<cmd>normal <Down><cr>", { buffer = 0 })
-          vim.keymap.set("n", "k", "<cmd>normal <Up><cr>", { buffer = 0 })
-          return buff
-        end,
-      }
-
-      starter.setup {
-        autoopen = true,
-        silent = true,
-        evaluate_single = true,
-
-        items = {
-          {
-            { name = "Files", action = function() require("snacks").picker.files() end, section = "" },
-            { name = "Git", action = function() require("astrocore").toggle_term_cmd "lazygit" end, section = "" },
-            { name = "Diff", action = "DiffviewOpen", section = "" },
-            { name = "Explorer", action = function() require("tfm").open() end, section = "" },
-          },
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      sections = {
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function() return require("snacks").git.get_root() ~= nil end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 2,
         },
-
-        header = "",
-
-        content_hooks = {
-          starter.gen_hook.adding_bullet(),
-          starter.gen_hook.aligning("center", "center"),
-          hooks.clear_statusline,
-          hooks.local_keymap,
-        },
-
-        query_updaters = "abcdefghilmnopqrstuvwxyz0123456789_-.",
-      }
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local padding = "░ "
-          starter.config.footer = padding .. stats.loaded .. "/" .. stats.count .. "  "
-          pcall(starter.refresh)
-        end,
-      })
-    end,
+        { section = "startup" },
+        { hidden = true, key = "q", action = ":qa" },
+      },
+    },
   },
 }
