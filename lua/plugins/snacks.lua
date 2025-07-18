8a1470809152894ceebf16efa74573e7b8b69387
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-07-18 14:18:15 +0330

    Add snacks notifier icons

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,16 +2,18 @@ return {
   "folke/snacks.nvim",
   opts = {
     notifier = {
+      margin = { top = 0, right = 0, bottom = 0 },
+      width = { min = 0, max = 0.4 },
+      padding = true,
       icons = {
-        error = "",
-        warn = "",
-        info = "",
-        debug = "",
-        trace = "",
+        error = "  ",
+        warn = "  ",
+        info = "  ",
+        debug = "  ",
+        trace = "  ",
       },
       keep = function(notif) return vim.fn.getcmdpos() > 0 end,
       style = "minimal",
-      more_format = " ↓ %d lines ",
     },
   },
 }
