008bc751167b01ac72e8efb0f8df3df32f5ef342
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-07-20 18:17:38 +0330

    Modify pickers

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -1,30 +1,45 @@
+local picker = require("snacks").picker
+
+local buffers = function()
+  picker.buffers {
+    finder = "buffers",
+    format = "buffer",
+    hidden = false,
+    unloaded = true,
+    current = false,
+    sort_lastused = true,
+    win = {
+      input = {
+        keys = {
+          ["<c-;>"] = { "bufdelete", mode = { "n", "i" } },
+          ["<Tab>"] = { "list_down", mode = { "n", "i" } },
+          ["<S-Tab>"] = { "list_up", mode = { "n", "i" } },
+        },
+      },
+      list = { keys = { ["dd"] = "bufdelete" } },
+    },
+  }
+end
+
 return {
   "folke/snacks.nvim",
   keys = {
+    { "<Tab>", mode = { "n" }, buffers, desc = "Buffers" },
     {
-      "<Tab>",
+      "<leader>fq",
       mode = { "n" },
       function()
-        require("snacks").picker.buffers {
-          finder = "buffers",
-          format = "buffer",
-          hidden = false,
-          unloaded = true,
-          current = false,
-          sort_lastused = true,
-          win = {
-            input = {
-              keys = {
-                ["<c-x>"] = { "bufdelete", mode = { "n", "i" } },
-                ["<Tab>"] = { "list_down", mode = { "n", "i" } },
-                ["<S-Tab>"] = { "list_up", mode = { "n", "i" } },
-              },
-            },
-            list = { keys = { ["dd"] = "bufdelete" } },
-          },
-        }
+        picker.qflist { on_show = function() vim.cmd.stopinsert() end }
       end,
-      desc = "Buffers",
+      desc = "Find qflist",
+    },
+    {
+      "<leader>fj",
+      mode = { "n" },
+      function()
+        picker.jumps { on_show = function() vim.cmd.stopinsert() end }
+      end,
+      desc = "Find jumps",
     },
   },
   opts = {
