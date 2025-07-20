0ed381b24183bb7310202b399c1489a586d852bc
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-07-20 18:22:41 +0330

    Update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -36,7 +36,7 @@ return {
           cmd = "git status --short --branch --renames",
           height = 5,
           padding = 1,
-          ttl = 5 * 60,
+          ttl = 60,
           indent = 2,
         },
         { section = "startup" },
