1abd45e9865ebe79e04e5f6c75ca23b8c46fb616
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-07-22 11:57:03 +0330

    Add open in split to mini files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -20,7 +20,48 @@ return {
       pattern = "MiniFilesBufferCreate",
       callback = function(args)
         local refresh = function() MiniFiles.refresh { content = { filter = dot_file_filter:toggle() } } end
-        vim.keymap.set("n", "H", refresh, { buffer = args.data.buf_id })
+        vim.keymap.set("n", "H", refresh, { buffer = args.data.buf_id, desc = "Toggle dot files" })
+      end,
+    })
+
+    local map_split = function(buf_id, lhs, direction)
+      local rhs = function()
+        local cur_target = MiniFiles.get_explorer_state().target_window
+        local new_target = vim.api.nvim_win_call(cur_target, function()
+          vim.cmd(direction .. " split")
+          return vim.api.nvim_get_current_win()
+        end)
+
+        MiniFiles.set_target_window(new_target)
+      end
+
+      local desc = "Split " .. direction
+      vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        map_split(buf_id, "<C-s>", "belowright horizontal")
+        map_split(buf_id, "<C-v>", "belowright vertical")
+      end,
+    })
+
+    local yank_path = function()
+      local path = (MiniFiles.get_fs_entry() or {}).path
+      if path == nil then return vim.notify "Cursor is not on valid entry" end
+      vim.fn.setreg(vim.v.register, path)
+    end
+
+    local ui_open = function() vim.ui.open(MiniFiles.get_fs_entry().path) end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local b = args.data.buf_id
+        vim.keymap.set("n", "gx", ui_open, { buffer = b, desc = "OS open" })
+        vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
       end,
     })
 
@@ -47,7 +88,7 @@ return {
 
       options = {
         permanent_delete = true,
-        use_as_default_explorer = false,
+        use_as_default_explorer = true,
       },
 
       windows = {
