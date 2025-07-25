4dd3a5e116a0fafc8c6c493f24f7db476be7c347
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-07-25 07:27:22 +0330

    Lazy load eldrich theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,7 +3,7 @@ return {
 
   { "projekt0n/github-nvim-theme", priority = 1000, lazy = true },
   { "Shatur/neovim-ayu", priority = 1000, lazy = true },
-  { "eldritch-theme/eldritch.nvim", priority = 1000, lazy = false, opts = {} },
+  { "eldritch-theme/eldritch.nvim", priority = 1000, lazy = true, opts = {} },
   { "catppuccin/nvim", priority = 1000, lazy = true, name = "catppuccin" },
   { "dracula/vim", priority = 1000, lazy = true, name = "dracula" },
   { "marko-cerovac/material.nvim", priority = 1000, lazy = true },
