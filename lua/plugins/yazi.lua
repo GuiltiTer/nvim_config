51acd1b9367f7fac8f919487a52dff078ca14d53
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-01 12:45:17 +0330

    Modify yazi's config

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -4,8 +4,8 @@ return {
   keys = { { "<leader>e", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open file manager" } },
   opts = {
     open_for_directories = true,
-    floating_window_scaling_factor = 1,
-    yazi_floating_window_border = "none",
+    floating_window_scaling_factor = 0.8,
+    yazi_floating_window_border = "rounded",
     keymaps = {
       show_help = "g?",
     },
