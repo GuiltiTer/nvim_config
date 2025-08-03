7bfb2f16c9fd4bf4f0c8a921d6b297240946f099
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-03 23:12:42 +0330

    feat(spectre): add Replace command to toggle it

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -1,18 +1,21 @@
 return {
   "nvim-pack/nvim-spectre",
-  cmd = "Spectre",
-  opts = {
-    open_cmd = "new",
-    default = { replace = { cmd = "sd" } },
-    mapping = {
-      ["quit"] = { map = "q", cmd = "<cmd>q<cr>", desc = "quit" },
-      ["quit2"] = { map = "<leader>q", cmd = "<cmd>q<cr>", desc = "quit" },
-      ["send_to_qf"] = {
-        map = "<c-q>",
-        cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
-        desc = "send all items to quickfix",
+  cmd = { "Spectre", "Replace" },
+  opts = function()
+    vim.api.nvim_create_user_command("Replace", "Spectre", { desc = "Replace" })
+    return {
+      open_cmd = "new",
+      default = { replace = { cmd = "sd" } },
+      mapping = {
+        ["quit"] = { map = "q", cmd = "<cmd>q<cr>", desc = "quit" },
+        ["quit2"] = { map = "<leader>q", cmd = "<cmd>q<cr>", desc = "quit" },
+        ["send_to_qf"] = {
+          map = "<c-q>",
+          cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+          desc = "send all items to quickfix",
+        },
       },
-    },
-  },
+    }
+  end,
   dependencies = { "nvim-lua/plenary.nvim" },
 }
