a2ad1101650052d89439067e96cf9a9f096da699
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-09 01:48:04 +0330

    style: cleanup astrolsp pluing configs

--- lua/plugins/astro/plugins/lsp.lua
+++ lua/plugins/astro/plugins/lsp.lua
@@ -10,65 +10,14 @@ return {
       semantic_tokens = true,
     },
     formatting = {
-      format_on_save = {
-        enabled = true,
-        ignore_filetypes = {},
-      },
-      disabled = {},
+      format_on_save = { enabled = true },
       timeout_ms = 5000,
     },
-    servers = {},
-    -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-      lua_ls = { settings = { Lua = { format = { enable = false } } } },
-    },
-    -- customize how language servers are attached
-    handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
-
-      -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-    },
-    -- Configure buffer local auto commands to add when attaching a language server
-    autocmds = {
-      -- first key is the `augroup` to add the auto commands to (:h augroup)
-      lsp_document_highlight = {
-        -- Optional condition to create/delete auto command group
-        -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-        -- condition will be resolved for each client on each execution and if it ever fails for all clients,
-        -- the auto commands will be deleted for that buffer
-        cond = "textDocument/documentHighlight",
-        -- cond = function(client, bufnr) return client.name == "lua_ls" end,
-        -- list of auto commands to set
-        {
-          -- events to trigger
-          event = { "CursorHold", "CursorHoldI" },
-          -- the rest of the autocmd options (:h nvim_create_autocmd)
-          desc = "Document Highlighting",
-          callback = function() vim.lsp.buf.document_highlight() end,
-        },
-        {
-          event = { "CursorMoved", "CursorMovedI", "BufLeave" },
-          desc = "Document Highlighting Clear",
-          callback = function() vim.lsp.buf.clear_references() end,
-        },
+      lua_ls = {
+        settings = { Lua = { format = { enable = false } } },
       },
     },
-    mappings = {
-      n = {
-        -- basics
-        ["<Leader>le"] = { "<cmd>LspRestart<cr>", desc = "Restart LSP" },
-      },
-    },
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    on_attach = function(client, bufnr)
-      -- this would disable semanticTokensProvider for all clients
-      -- client.server_capabilities.semanticTokensProvider = nil
-    end,
   },
 }
