32f7dae8dbe9463248dc5c614d118caa86486cb1
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-10 15:24:38 +0330

    feat(mapping): add terminal escape with double <esc>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,3 +4,5 @@ key({ "n", "x", "v" }, "<C-p>", '"0p', { desc = "Put last yanked" })
 key({ "n", "x", "v" }, "H", "^", { desc = "Beginning of the line" })
 key({ "n", "x", "v" }, "L", "$", { desc = "End of the line" })
 key({ "n", "x", "v" }, "<leader>w", "<cmd>wa<cr>", { desc = "Write all" })
+
+key({ "t" }, "<esc><esc>", "<c-\\><c-n>", { desc = "Escapce" })
