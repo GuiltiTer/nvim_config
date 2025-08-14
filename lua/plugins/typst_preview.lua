8da59645ab5b00804c1911e586449a1e7d506936
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-15 01:47:04 +0330

    feat(typst-preview): add local preview file keymap

--- lua/plugins/typst_preview.lua
+++ lua/plugins/typst_preview.lua
@@ -1,6 +1,7 @@
 return {
   "chomosuke/typst-preview.nvim",
   ft = { "typst" },
+  keys = { { mode = { "n" }, "<leader>rp", "<cmd>TypstPreview<cr>", desc = "Preview file", ft = { "typst" } } },
   version = "1.*",
   opts = {
     dependencies_bin = { ["tinymist"] = "tinymist" },
