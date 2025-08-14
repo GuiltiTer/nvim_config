b6b1116668a55be04c7b5b9f7ad2d536de4791ad
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-15 00:07:49 +0330

    fix(yazi): remove round border from its floating window

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,20 +1,18 @@
 return {
   "mikavilpas/yazi.nvim",
-  event = "VeryLazy",
   keys = { { "<leader>e", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open file manager" } },
   opts = {
     open_for_directories = true,
     floating_window_scaling_factor = 1,
-    yazi_floating_window_border = "rounded",
+    yazi_floating_window_border = "none",
     keymaps = {
       show_help = "g?",
     },
   },
-  -- 👇 if you use `open_for_directories=true`, this is recommended
   init = function()
+    -- if you use `open_for_directories=true`, this is recommended
     -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-    -- vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+    vim.g.loaded_netrwPlugin = 1 -- mark netrw as loaded so it's not loaded at all.
   end,
   dependencies = { { "nvim-lua/plenary.nvim", lazy = true } },
 }
