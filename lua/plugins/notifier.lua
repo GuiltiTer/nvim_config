62209f1063b4d3237033b11c056bd5ebc7f72aef
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-17 14:28:47 +0330

    feat(notifier): better style

--- lua/plugins/notifier.lua
+++ lua/plugins/notifier.lua
@@ -6,14 +6,20 @@ return {
       width = { min = 0, max = 0.4 },
       padding = true,
       icons = {
-        error = "  ",
-        warn = "  ",
-        info = "  ",
-        debug = "  ",
-        trace = "  ",
+        error = " ",
+        warn = " ",
+        info = " ",
+        debug = " ",
+        trace = " ",
       },
       keep = function(notif) return vim.fn.getcmdpos() > 0 end,
-      style = "minimal",
+
+      style = function(buf, notif, ctx)
+        local title = vim.trim((notif.title or "") .. " " .. notif.icon)
+        ctx.opts.title = { { " " .. title .. " ", ctx.hl.title } }
+        ctx.opts.title_pos = "right"
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(notif.msg, "\n"))
+      end,
     },
   },
 }
