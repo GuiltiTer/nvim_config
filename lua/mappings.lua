8b19c0851b280238f7df4b0a809fd9b03de28f34
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-18 01:42:09 +0330

    feat(mapping): add buffer alternate keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,7 @@ key({ "n", "x", "v" }, "<C-p>", '"0p', { desc = "Put last yanked" })
 key({ "n", "x", "v" }, "H", "^", { desc = "Beginning of the line" })
 key({ "n", "x", "v" }, "L", "$", { desc = "End of the line" })
 key({ "n", "x", "v" }, "<leader>w", "<cmd>wa<cr>", { desc = "Write all" })
+key({ "n", "x", "v" }, "<c-;>", "<c-^>", { desc = "Alternate buffer" })
 
 key({ "t" }, "<esc><esc>", "<c-\\><c-n>", { desc = "Escapce" })
 
