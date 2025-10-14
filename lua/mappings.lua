475f89407cf397477bab56da9bc7c7e7955a8ff6
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-10-15 00:42:18 +0330

    feat(mapping): add yank and put using registers mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,7 +3,8 @@ local group = function(mode, lhs, desc, opts)
   key(mode, lhs, "<Nop>", vim.tbl_deep_extend("keep", { desc = desc }, opts and opts or {}))
 end
 
-key({ "n", "x", "v" }, "<C-p>", '"0p', { desc = "Put last yanked" })
+key({ "n", "x", "v" }, "<C-p>", '"zp', { desc = "Put from register z" })
+key({ "n", "x", "v" }, "<C-y>", '"zy', { desc = "Yank to register z" })
 key({ "n", "x", "v" }, "H", "^", { desc = "Beginning of the line" })
 key({ "n", "x", "v" }, "L", "$", { desc = "End of the line" })
 key({ "n", "x", "v" }, "<leader>w", "<cmd>wa<cr>", { desc = "Write all" })
