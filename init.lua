1b438d343d4f076eb951d094f58812c25db934b8
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-10-22 20:59:46 +0330

    refactor: move lazy setup to its file

--- init.lua
+++ init.lua
@@ -1,34 +1,3 @@
--- stylua: ignore start
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
-if not pcall(require, "lazy") then
-  vim.api.nvim_echo( { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-  vim.fn.getchar()
-  vim.cmd.quit()
-end
--- stylua: ignore end
-
-require("lazy").setup({
-  { import = "plugins" },
-} --[[@as LazySpec]], {
-  ui = { backdrop = 100 },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "zipPlugin",
-      },
-    },
-  },
-} --[[@as LazyConfig]])
-
+require "lazy_setup"
 require "polish"
 require "mappings"
