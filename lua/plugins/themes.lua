fca86f397aeda051a453b50da6504227507a6727
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-11-11 16:08:24 +0330

    feat(theme): add neofusion

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,5 +1,6 @@
 return {
   { "folke/tokyonight.nvim", priority = 1000, lazy = false },
+  { "diegoulloao/neofusion.nvim", priority = 1000, lazy = true, config = true },
   { "projekt0n/github-nvim-theme", priority = 1000, lazy = true },
   { "catppuccin/nvim", priority = 1000, lazy = true, name = "catppuccin" },
   { "marko-cerovac/material.nvim", priority = 1000, lazy = true },
