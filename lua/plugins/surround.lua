aa524b79fd398ac41f126312755ef2a556ebc0b7
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-05-19 22:44:54 +0330

    feat(surround): update to v4

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,14 +1,13 @@
 return {
   "kylechui/nvim-surround",
   version = "*",
-  event = "BufRead",
-  opts = {
-    keymaps = {
-      normal = "s",
-      normal_cur = "ss",
-      visual = "s",
-      delete = "ds",
-      change = "cs",
-    },
+  keys = {
+    { "s", mode = { "n" }, "<Plug>(nvim-surround-normal)", desc = "Surround" },
+    { "ss", mode = { "n" }, "<Plug>(nvim-surround-normal-cur-line)", desc = "Surround line" },
+    { "s", mode = { "x" }, "<Plug>(nvim-surround-visual)", desc = "Surround visual" },
+    { "ds", mode = { "n" }, "<Plug>(nvim-surround-delete)", desc = "Surround delete" },
+    { "cs", mode = { "n" }, "<Plug>(nvim-surround-change)", desc = "Surround change" },
   },
+  event = "BufRead",
+  opts = {},
 }
