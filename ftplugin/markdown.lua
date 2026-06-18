a941885fa3708d31cc17830f37d24b27dec07772
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-06-18 22:35:40 +0330

    style: fix typo

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -3,6 +3,6 @@ local pandoc = require "user.pandoc"
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    vim.keymap.set({ "n" }, "<localleader>b", function() pandoc.build { auto_open = true } end, { desc = "Buld file" })
+    vim.keymap.set({ "n" }, "<localleader>b", function() pandoc.build { auto_open = true } end, { desc = "Build file" })
   end,
 })
